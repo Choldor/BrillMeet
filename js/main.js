66819ddd945080f3737719a24f8d808b5329e1d1
Choldor <choldor123@gmail.com>
2020-07-13 14:07:24 +0200

    pon

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
-const hamburger = document.querySelector('.hamburger')
-const navMobile = document.querySelector('.nav-items')
+
+const hamburger = document.querySelector('.hamburger');
+const navMobile = document.querySelector('.nav-items');
+const nav = document.querySelector('nav');
 const handleClick = () => {
     hamburger.classList.toggle('hamburger-active');
     navMobile.classList.toggle('nav-items-active');
 }
 
 hamburger.addEventListener('click', handleClick);
+
+
+const scrollNav = () => {
+    if (nav.offsetTop  < 50) {
+        nav.classList.add(".nav-background")
+    }
+    else {
+        nav.style.background = "transparent";
+}
+}
+window.addEventListener('scroll', function() {
+    if (nav.offsetTop < 30){
+    nav.style.background = "red";}
+    else {nav.style.background = "transparent";}
+    });
